45f11b97e8042d1b599e867cce3619b61143f6bb
DragonMaster436 <96381483+DragonMaster436@users.noreply.github.com>
2024-02-17 18:40:25 -0600

    Add files via upload

---
+++ Day-2-SandBox.py
@@ -0,0 +1,26 @@
+# print("Hello"[0,2])
+# the [] is helping me geting just one letter out of a string
+# num = 123_456_789
+# print(num)
+# num = input("What number? ")
+# print(type(num))
+# this down here changes num from string into float
+# new_num = float(num)
+# print(type(new_num))
+# inputs work with floats it actually turns it into a string
+#  + addition
+#  - subtraction
+#  * mutiplacation
+#  / division 
+#  ** to the power/exponets
+# Python follows PEMDAS
+# print(3 * 3 / 3 + 3 - 3) 
+#  or put () round the 3 + 3 instead of swaping / from +
+# print(type(round(8 / 3)))
+# the round variable rounds the numbers togetherI could also just put the // in there instead and it will not
+# round but automaticly turn a flowting point value into a int 
+#  print(8 // 3)
+# / always float point // always int
+
+# f-string
+# f"words in here{int/boolean/other}"
